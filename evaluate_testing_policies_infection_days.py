9acf7cbb6d216360bbd91f43067948cccb17607f
Patricio Foncea Araneda <foncea@mit.edu>
2021-05-24 15:25:47 -0400

    update and new results plots

--- evaluate_testing_policies_infection_days.py
+++ evaluate_testing_policies_infection_days.py
@@ -17,7 +17,7 @@ import time
 
 # Global Vars
 
-T = 10
+T = 8
 
 # Methods
 
@@ -28,6 +28,17 @@ def count_infecting(df, t):
   
   c = str(t)
   aux = df.loc[df[c] >= 6]
+  return aux.shape[0]
+
+def count_infecting_corr(df, t, s):
+  '''
+  Count number of viral load curves in df that are infecting at day t AND also at day s.
+  Method necessary to compute the variance of the estimator (covariance across time).
+  '''
+  
+  c_t = str(t)
+  c_s = str(s)
+  aux = df[(df[c_t] >= 6) & (df[c_s] >= 6)]
   return aux.shape[0]
 
 def filter_antigent_tested(df, t):
@@ -45,6 +56,13 @@ def filter_pcr_tested(df, t):
   
   c = str(t - 1)
   return df.loc[df[c] < 3]
+
+def filter_symptomatic(df, t):
+  '''
+  Filter out symptomatic individuals
+  '''
+  
+  return df[(df['14'] > t) | (df['14'] < 1)] 
 
 def generate_budgeted_policies(num_antigen, num_pcr):
   '''
@@ -70,7 +88,8 @@ def evaluate_policy(df, policy):
   
   prob_inf = 0
   
-  for t in range(1, T + 1):
+  for t in range(0, T + 1):
+    aux = filter_symptomatic(aux, t)
     if t - 1 in D_a: 
       aux = filter_antigent_tested(aux, t - 1)
     if t - 1 in D_p:
@@ -81,7 +100,7 @@ def evaluate_policy(df, policy):
     if den != 0:
       prob_inf += num / num_mc #den
     
-  return prob_inf
+  return prob_inf  
 
 def evaluate_policy_lag(df, policy, t_l=0):
   '''
@@ -95,22 +114,54 @@ def evaluate_policy_lag(df, policy, t_l=0):
   D_a = [d + t_l for d in policy[0]]
   D_p = [d + t_l for d in policy[1]]
   
-  prob_inf = 0
-  work_days = 0
+  prob_inf = []  # (1): Expected number of infecting days
+  work_days = [] # (2): Expected number of non-infecting days
+  
+  variance_pi = 0 # (3): Variance of (1)
+  variance_wd = 0 # (4): Variance of (2)
+  
+  # (5): x% quantile of (1)
+  quantile_pi = {.95: 0,
+                 .90: 0,
+                 .80: 0,
+                 .50: 0,
+                 .20: 0,
+                 .10: 0,
+                 .05: 0,}
+  # (6): x% quantile of (2)
+  quantile_wd = {.95: 0,
+                 .90: 0,
+                 .80: 0,
+                 .50: 0,
+                 .20: 0,
+                 .10: 0,
+                 .05: 0}
   
   for t in range(0, 14):
-    if t - 1 in D_a: 
-      aux = filter_antigent_tested(aux, t - 1)
-    if t - 1 in D_p:
-      aux = filter_pcr_tested(aux, t - 1)
+    aux = filter_symptomatic(aux, t)
+    if t in D_a: 
+      aux = filter_antigent_tested(aux, t)
+    if t in D_p:
+      aux = filter_pcr_tested(aux, t)
       
     den = aux.shape[0]
     if den != 0:
       num = count_infecting(aux, t)
-      prob_inf += num / num_mc
-      work_days += den / num_mc
-    
-  return prob_inf, work_days
+      prob_inf.append(num / num_mc)
+      work_days.append((den - num) / num_mc)
+      
+      variance_pi += prob_inf[t] * (1 - prob_inf[t]) 
+      variance_wd += work_days[t] * (1 - work_days[t])
+      for s in range(t):
+        num_tau = count_infecting_corr(aux, t, s)
+        variance_pi += 2 * (num_tau / num_mc - prob_inf[t] * prob_inf[s])
+        variance_wd += 2 * ((den - num) / num_mc - work_days[t] * work_days[s])
+      
+      for q in quantile_pi:
+        quantile_pi[q] = quantile_pi[q] + (prob_inf[t] >= 1 - q)
+        quantile_wd[q] = quantile_wd[q] + (work_days[t] >= 1 - q)
+      
+  return sum(prob_inf), sum(work_days), variance_pi, variance_wd, quantile_pi, quantile_wd
 
 def weight_policy_symptom_day(df, policy):
   '''
@@ -129,14 +180,34 @@ def weight_policy_symptom_day(df, policy):
   prob_inf = []
   work_days = []
   
+  variance_pi = []
+  variance_wd = []
+  
+  quantile_pi = []
+  quantile_wd = []
+
   for d in inf_day_pdf:
-    pi, wd = evaluate_policy_lag(df, policy, t_l=d)
+    pi, wd, v_pi, v_wd, q_pi, q_wd = evaluate_policy_lag(df, policy, t_l=d)
+    
     prob_inf.append(pi)
     work_days.append(wd)
+    
+    variance_pi.append(v_pi)
+    variance_wd.append(v_wd)
+    
+    quantile_pi.append(q_pi)
+    quantile_wd.append(q_wd)
   
   prob_inf_w = sum([prob_inf[d] * inf_day_pdf[d] for d in inf_day_pdf])
   work_days_w = sum([work_days[d] * inf_day_pdf[d] for d in inf_day_pdf])
-  return prob_inf_w, work_days_w
+  
+  variance_pi_w = sum([variance_pi[d] * inf_day_pdf[d] for d in inf_day_pdf])
+  variance_wd_w = sum([variance_wd[d] * inf_day_pdf[d] for d in inf_day_pdf])
+  
+  quantile_pi_w = {q: sum([quantile_pi[d][q] * inf_day_pdf[d] for d in inf_day_pdf]) for q in q_pi}
+  quantile_wd_w = {q: sum([quantile_wd[d][q] * inf_day_pdf[d] for d in inf_day_pdf]) for q in q_wd}
+  
+  return prob_inf_w, work_days_w, variance_pi_w, variance_wd_w, quantile_pi_w, quantile_wd_w
 
 def evaluate_budgeted_policy(df, num_antigen, num_pcr):
   '''
@@ -147,13 +218,19 @@ def evaluate_budgeted_policy(df, num_antigen, num_pcr):
   policy_cost = []
   
   for p in policies:
-    prob_inf, work_days = weight_policy_symptom_day(df, p)
+    prob_inf, work_days, variance_pi, variance_wd, quantile_pi, quantile_wd = weight_policy_symptom_day(df, p)
     aux_df = pd.DataFrame({'num_antigen': num_antigen,
                            'num_pcr': num_pcr,
                            'antigen_days': [[d for d in p[0]]],
                            'pcr_days': [[d - 1 for d in p[1]]],
                            'expected_infecting_days': prob_inf,
-                           'expected_non_infecting_days': work_days - prob_inf})
+                           'variance_infecting_days': variance_pi,
+                           'expected_non_infecting_days': work_days,
+                           'variance_non_infecting_days': variance_wd})
+    aux_q_pi = {'infecting_days_' + str(d): [quantile_pi[d]] for d in quantile_pi}
+    aux_q_wd = {'non_infecting_days_' + str(d): [quantile_wd[d]] for d in quantile_wd}
+    aux_df = aux_df.join(pd.DataFrame(aux_q_pi)).join(pd.DataFrame(aux_q_wd))
+    
     policy_cost.append(aux_df)
   
   return policy_cost
@@ -161,7 +238,7 @@ def evaluate_budgeted_policy(df, num_antigen, num_pcr):
 # Main
 
 def main2():
-  df = pd.read_csv('viral_load_mc.csv').iloc[:int(1e6)]
+  df = pd.read_csv('data/viral_load_mc.csv').iloc[:int(1e6)]
   
   policy_space_limits = {'antigen': [0, 5], 
                          'pcr': [0, 2]}
@@ -180,24 +257,7 @@ def main2():
       
       print(str((num_antigen, num_pcr)) + ' finished in ' + str((time.time() - start_time) / 60))
             
-  pd.concat(policy_cost).to_csv('results_18_05_1.csv')
-  
-def main():
-  df = pd.read_csv('viral_load_mc.csv').iloc[:int(1e5)]
-  
-  policies = generate_budgeted_policies(2, 1)
-  policy_cost = []
-  for p in policies:
-    # prob_inf = evaluate_policy(df, p)
-    prob_inf, work_days = weight_policy_symptom_day(df, p)
-    aux_df = pd.DataFrame({'antigen_days': [p[0]],
-                           'pcr_days': [[d - 1 for d in p[1]]],
-                           'expected_infecting_days': prob_inf,
-                           'expected_non_infecting_days': work_days - prob_inf})
-    policy_cost.append(aux_df)
-  
-  pd.concat(policy_cost).to_csv('results5.csv')
-
+  pd.concat(policy_cost).to_csv('data/results_23_05_2.csv')
 
 # Execute
 
--- plots.r
+++ plots.r
@@ -2,7 +2,7 @@ library(tidyverse)
 library(plotly)
 
 # Read Data
-df = read.csv('data/results_17_05_4.csv')
+df = read.csv('data/results_23_05_2.csv')
 
 # Plot 1: facet_grid with all points
 df %>% mutate(num_antigen = paste('num_antigen:', num_antigen),
@@ -29,24 +29,52 @@ df %>% arrange(expected_infecting_days, expected_non_infecting_days) %>%
   geom_point()
 
 # Plot 3: Interactive with best points
-df_aux = df %>% arrange(expected_infecting_days, expected_non_infecting_days) %>%
-  group_by(num_pcr, num_antigen) %>%
-  summarize(expected_infecting_days = first(expected_infecting_days),
-            expected_non_infecting_days = first(expected_non_infecting_days),
+df_aux = df %>% 
+  arrange(expected_infecting_days, expected_non_infecting_days) %>%
+  mutate(Policy_Budget = paste('Antigen: ', num_antigen, '- PCR: ', num_pcr),
+         num_a = paste('Antigen:', num_antigen),
+         num_p = paste('PCR:', num_pcr)) %>%
+  group_by(num_pcr, num_antigen, Policy_Budget, num_a, num_p) %>%
+  summarize(expected_infecting_days = round(first(expected_infecting_days), 2),
+            expected_non_infecting_days = round(first(expected_non_infecting_days), 2),
             p_A = first(antigen_days),
-            p_P = first(pcr_days)) %>%
-  filter(num_pcr + num_antigen > 0)
+            p_P = first(pcr_days)) 
 
-df_aux %>% plot_ly(text = ~paste('num_A = ', num_antigen, ', num_P = ', num_pcr, '\npi_A = ', p_A, '\npi_P = ', p_P),
-                         x = ~expected_infecting_days,
-                         y = ~expected_non_infecting_days,
-                         color = ~interaction(num_antigen, num_pcr))
+df_aux %>% 
+  plot_ly(text = ~paste('(A, P) = (', num_antigen, ', ', num_pcr, ')', sep=''),
+          x = ~expected_infecting_days,
+          y = ~expected_non_infecting_days,
+          color = ~Policy_Budget,
+          mode = 'line+markers',
+          marker = list(size=8))
+
+df_aux %>% 
+  plot_ly(text = ~paste('(A, P) = (', num_antigen, ', ', num_pcr, ')', sep=''),
+          x = ~expected_infecting_days,
+          y = ~expected_non_infecting_days,
+          color = ~num_a,
+          symbol = ~num_p,
+          symbols = c('0', 'circle', 'cross'),
+          mode = 'markers',
+          marker = list(size=12))
+
+df_aux %>% 
+  group_by(num_p) %>% 
+  do(p = plot_ly(., text = ~paste('num_A = ', num_antigen, ', num_P = ', num_pcr, '\npi_A = ', p_A, '\npi_P = ', p_P),
+          x = ~expected_infecting_days,
+          y = ~expected_non_infecting_days,
+          color = ~num_a,
+          mode = 'markers',
+          marker = list(size=8))) %>% 
+  subplot(nrows = 3, shareX=TRUE)
 
 # Plot 4: Frontier for all budgets together
-pareto_df = read.csv('data/pareto_points_18_05_1.csv')
+pareto_df = read.csv('data/pareto_points_23_05_2.csv')
 
 pareto_df %>% 
-  mutate(Policy_Budget = paste('Antigen: ', num_antigen, '- PCR: ', num_pcr)) %>%
+  mutate(Policy_Budget = paste('Antigen: ', num_antigen, '- PCR: ', num_pcr),
+         num_a = paste('Antigen:', num_antigen),
+         num_p = paste('PCR:', num_pcr)) %>%
   plot_ly(text = ~paste('num_A = ', num_antigen, ', num_P = ', num_pcr, '\n',
                         'pi_A = ', antigen_days, '\n',
                         'pi_P = ', pcr_days),
@@ -72,5 +100,45 @@ pareto_df %>%
           symbols = c('0', 'circle', 'cross'),
           mode = 'lines+markers',
           marker = list(size=8),
-          line = list(shape='vh')
+          line = list(shape='hv')
           )
+
+# Plot 5: Percentile interval per policy
+
+df %>% 
+  arrange(expected_infecting_days, -expected_non_infecting_days) %>% 
+  group_by(num_antigen, num_pcr) %>% 
+  summarize(expected_infecting_days = first(expected_infecting_days),
+            q80 = first(infecting_days_0.8),
+            q20 = first(infecting_days_0.2),
+            antigen_days = first(antigen_days),
+            pcr_days = first(pcr_days)) %>% 
+  mutate(budget_policy = paste('(', num_antigen, ', ', num_pcr, ')', sep=''),
+         lower_error = expected_infecting_days - q20,
+         upper_error = q80 - expected_infecting_days) %>% 
+  ggplot(aes(x=budget_policy, y=expected_infecting_days)) +
+  geom_point() +
+  geom_errorbar(aes(ymin=q20, ymax=q80)) +
+  xlab('(# Antigen, # PCR)')
+
+aux = df %>% 
+  arrange(expected_infecting_days, -expected_non_infecting_days) %>% 
+  group_by(num_antigen, num_pcr) %>% 
+  summarize(expected_infecting_days = first(round(expected_infecting_days, 2)),
+            q80 = first(round(infecting_days_0.8, 2)),
+            q20 = first(round(infecting_days_0.2, 2)),
+            antigen_days = first(antigen_days),
+            pcr_days = first(pcr_days)) %>% 
+  mutate(budget_policy = paste('(', num_antigen, ', ', num_pcr, ')', sep=''),
+         lower_error = expected_infecting_days - q20,
+         upper_error = q80 - expected_infecting_days) %>% 
+  plot_ly(type = 'scatter',
+          text = ~paste('A: ', antigen_days, ', P: ', pcr_days),
+          x = ~budget_policy,
+          y = ~expected_infecting_days,
+          error_y = ~list(symmetric=FALSE,
+                          arrayminus= lower_error,
+                          array= upper_error,
+                          width = 10)
+  )
+aux  
